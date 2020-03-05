ec353f07a82f349babf0735cf42feb834530da23
gusmagl <gustavo.malamud@globallogic.com>
2020-03-05 02:08:16 -0300

    Adds about updated page

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,9 +14,41 @@ class About extends React.Component {
 					<section id='one'>
 						<div className='inner'>
 							<header className='major'>
-								<h1>Under construction</h1>
+								<h1>About Gustavo Malamud</h1>
 							</header>
-							¡ <p>This will be written shortly. I promise.</p>
+							<h2>
+								Hi. Sorry to annoy you with an overused phrase. I'm Gustavo.
+							</h2>
+							<p>
+								I'm 36 years old, from Argentina. I've recently begun my career
+								as a Front End Web Developer after working 14 years in the
+								Digital Marketing industry. No terrible regrets about it, just
+								needed a change.
+							</p>
+							<p>
+								Currently on a process to rediscover my curiosity and abilities
+								as a developer. Currently learning React, Redux, Node and pretty
+								much everything I can get my hands on and what{" "}
+								<strike>very limited</strike> time affords me.
+							</p>
+							<p>
+								I must confess I have been suffering from some form of Impostor
+								Syndrome symptoms lately, but fair and square, from what I've
+								heard, everyone suffers from it at some point. I just wish{" "}
+								<i>it doesn't materialise as true in my case.</i>
+							</p>
+							<p>
+								Items of expertice? I'm quite good at HTML, some PHP, CSS/ CSS3,
+								some CMS's (Drupal, Wordpress) that I've worked with, and
+								lately, React and Gatsby :)
+							</p>
+							<p>
+								Have some project I can help you with?{" "}
+								<i>Need advisory on making a solid argentinian barbecue?</i>{" "}
+								Could use my help on your projects? Wish to buy me a beer, a
+								coffee or some chill pills?{" "}
+								<Link to='/contact'>Get in touch with me!</Link>
+							</p>
 							<p>
 								<Link to='/'>Back to home page.</Link>
 							</p>
