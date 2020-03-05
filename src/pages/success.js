cc14bbe93b0b3a7d9700d6b83971630aa3e998e9
gusmagl <gustavo.malamud@globallogic.com>
2020-03-05 00:24:42 -0300

    Fix success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,24 +4,31 @@ import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const Success = props => (
-	<Layout location={this.props.location} title={siteTitle}>
-		<div id='main' className='alt'>
-			<section id='one'>
-				<div className='inner'>
-					<header className='major'>
-						<h1>Success/Thank You Page</h1>
-					</header>
-					<span className='image main'></span>
-					<p>Thank you for contacting me!</p>
-					<p>
-						<Link to='/'>Back to home page.</Link>
-					</p>
+class Success extends React.Component {
+	render() {
+		const { data } = this.props;
+		const siteTitle = data.site.siteMetadata.title;
+
+		return (
+			<Layout location={this.props.location} title={siteTitle}>
+				<div id='main' className='alt'>
+					<section id='one'>
+						<div className='inner'>
+							<header className='major'>
+								<h1>Success/Thank You Page</h1>
+							</header>
+							<span className='image main'></span>
+							<p>Thank you for contacting me!</p>
+							<p>
+								<Link to='/'>Back to home page.</Link>
+							</p>
+						</div>
+					</section>
 				</div>
-			</section>
-		</div>
-	</Layout>
-);
+			</Layout>
+		);
+	}
+}
 
 export default Success;
 
