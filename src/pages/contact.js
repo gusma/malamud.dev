7f2cc286fe72df14c921e399164fff9d7be969d6
gusmagl <gustavo.malamud@globallogic.com>
2020-03-05 00:46:40 -0300

    Fixes error on styling for contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import "contact.css";
+import "./contact.css";
 
 import { rhythm } from "../utils/typography";
 
