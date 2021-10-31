a01b2ebaeff7d9467360483f0c205bb3998ee22f
Gustavo Malamud <gustavo.malamud@gmail.com>
2021-10-31 07:08:26 -0300

    Updates libraries, redoes pages in general

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,52 +7,48 @@
 
 import React from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
-import Image from "gatsby-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 import { rhythm } from "../utils/typography";
 
 const Bio = () => {
-	const data = useStaticQuery(graphql`
-		query BioQuery {
-			avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-				childImageSharp {
-					fixed(width: 50, height: 50) {
-						...GatsbyImageSharpFixed
-					}
-				}
-			}
-			site {
-				siteMetadata {
-					author
-					social {
-						twitter
-					}
-				}
-			}
-		}
-	`);
+	const data = useStaticQuery(graphql`query BioQuery {
+  avatar: file(absolutePath: {regex: "/profile-pic.jpg/"}) {
+    childImageSharp {
+      gatsbyImageData(width: 50, height: 50, layout: FIXED)
+    }
+  }
+  site {
+    siteMetadata {
+      author
+      social {
+        twitter
+      }
+    }
+  }
+}
+`);
 
 	const { author } = data.site.siteMetadata;
 	return (
-		<div
+        <div
 			style={{
 				display: `flex`,
 				marginBottom: rhythm(2.5)
 			}}
 		>
-			<Image
-				fixed={data.avatar.childImageSharp.fixed}
-				alt={author}
-				style={{
+			<GatsbyImage
+                image={data.avatar.childImageSharp.gatsbyImageData}
+                alt={author}
+                style={{
 					marginRight: rhythm(1 / 2),
 					marginBottom: 0,
 					minWidth: 50,
 					borderRadius: `100%`
 				}}
-				imgStyle={{
+                imgStyle={{
 					borderRadius: `50%`
-				}}
-			/>
+				}} />
 
 			<p>
 				Written by <strong>Gustavo Malamud</strong> who lives and works in
@@ -64,7 +60,7 @@ const Bio = () => {
 			</p>
 			<hr />
 		</div>
-	);
+    );
 };
 
 export default Bio;
