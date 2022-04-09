0369e2316a5d20da6e8ef7799666619bd1537973
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-04-09 03:48:44 -0300

    Fix issue with markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,105 +1,89 @@
 module.exports = {
-  siteMetadata: {
-    title: `New at this, at 38`,
-    author: `Gustavo Malamud`,
-    description: `Gustavo Malamud is a Front-End Developer slash Digital Marketer from Buenos Aires, Argentina. 🇦🇷 `,
-    siteUrl: `https://malamud.dev/`,
-    social: {
-      twitter: `gusma`,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    "gatsby-plugin-dark-mode",
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-vscode`,
-            options: {
-              theme: {
-								default: 'Solarized Light',
-								dark: 'Monokai Dimmed'
-							}
-            },
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-GL9K1604D6",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "malamud.dev",
-        // defaults to false
-        enableWebVitalsTracking: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+	siteMetadata: {
+		title: `New at this, at 38`,
+		author: `Gustavo Malamud`,
+		description: `Gustavo Malamud is a Front-End Developer slash Digital Marketer from Buenos Aires, Argentina. 🇦🇷 `,
+		siteUrl: `https://malamud.dev/`,
+		social: {
+			twitter: `gusma`,
+		},
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/blog`,
+				name: `blog`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/assets`,
+				name: `assets`,
+			},
+		},
+		'gatsby-plugin-dark-mode',
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 590,
+						},
+					},
+					{
+						resolve: `gatsby-remark-responsive-iframe`,
+						options: {
+							wrapperStyle: `margin-bottom: 1.0725rem`,
+						},
+					},
+					`gatsby-remark-prismjs`,
+					`gatsby-remark-copy-linked-files`,
+					`gatsby-remark-smartypants`,
+				],
+			},
+		},
+		`gatsby-plugin-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+			  // The property ID; the tracking code won't be generated without it
+			  trackingId: "G-GL9K1604D6",
+			  // Defines where to place the tracking script - `true` in the head and `false` in the body
+			  head: false,
+			  // Setting this parameter is optional
+			  anonymize: true,
+			  // Setting this parameter is also optional
+			  respectDNT: true,
+			  // Avoids sending pageview hits from custom paths
+			  exclude: ["/preview/**", "/do-not-track/me/too/"],
+			  // Delays sending pageview hits on route update (in milliseconds)
+			  pageTransitionDelay: 0,
+			  // Enables Google Optimize using your container Id
+			  optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+			  // Enables Google Optimize Experiment ID
+			  experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+			  // Set Variation ID. 0 for original 1,2,3....
+			  variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+			  // Defers execution of google analytics script after page load
+			  defer: false,
+			  // Any additional optional fields
+			  sampleRate: 5,
+			  siteSpeedSampleRate: 10,
+			  cookieDomain: "malamud.dev",
+			  // defaults to false
+			  enableWebVitalsTracking: true,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+			  query: `
 				{
 				  site {
 					siteMetadata {
@@ -111,20 +95,20 @@ module.exports = {
 				  }
 				}
 			  `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map((node) => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                });
-              });
-            },
-            query: `
+			  feeds: [
+				{
+				  serialize: ({ query: { site, allMarkdownRemark } }) => {
+					return allMarkdownRemark.nodes.map(node => {
+					  return Object.assign({}, node.frontmatter, {
+						description: node.excerpt,
+						date: node.frontmatter.date,
+						url: site.siteMetadata.siteUrl + node.fields.slug,
+						guid: site.siteMetadata.siteUrl + node.fields.slug,
+						custom_elements: [{ "content:encoded": node.html }],
+					  })
+					})
+				  },
+				  query: `
 					{
 					  allMarkdownRemark(
 						sort: { order: DESC, fields: [frontmatter___date] },
@@ -143,59 +127,59 @@ module.exports = {
 					  }
 					}
 				  `,
-            output: "/rss.xml",
-            title: "Your Site's RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/blog/",
-            // optional configuration to specify external rss feed, such as feedburner
-            link: "https://feeds.feedburner.com/gatsby/blog",
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-WQKPX65",
+				  output: "/rss.xml",
+				  title: "Your Site's RSS Feed",
+				  // optional configuration to insert feed reference in pages:
+				  // if `string` is used, it will be used to create RegExp and then test if pathname of
+				  // current page satisfied this regular expression;
+				  // if not provided or `undefined`, all pages will have feed reference inserted
+				  match: "^/blog/",
+				  // optional configuration to specify external rss feed, such as feedburner
+				  link: "https://feeds.feedburner.com/gatsby/blog",
+				},
+			  ],
+			},
+		  },		
+		  {
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Gatsby Starter Blog`,
+				short_name: `GatsbyJS`,
+				start_url: `/`,
+				background_color: `#ffffff`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `content/assets/gatsby-icon.png`,
+			},
+		},
+		`gatsby-plugin-offline`,
+		`gatsby-plugin-sitemap`,
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-plugin-typography`,
+			options: {
+				pathToConfigModule: `src/utils/typography`,
+			},
+		},
+		{
+			resolve: "gatsby-plugin-google-tagmanager",
+			options: {
+				id: "GTM-WQKPX65",
 
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+				// Include GTM in development.
+				// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
 
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
+				// datalayer to be set before GTM is loaded
+				// should be an object or a function that is executed in the browser
+				// Defaults to null
+				defaultDataLayer: { platform: "gatsby" },
 
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
-      },
-    },
-  ],
+				// Specify optional GTM environment details.
+				gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+				gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+				dataLayerName: "YOUR_DATA_LAYER_NAME",
+			},
+		},
+	],
 };
